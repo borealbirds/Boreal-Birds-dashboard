8e205651e6d01e0466e188901744940736040262
Wesley Beard <wes_beard@hotmail.com>
2026-05-20 17:36:22 -0700

    refactor: make sidebar layout and add sidebar here

--- app/ui/model_v5.py
+++ app/ui/model_v5.py
@@ -1,6 +1,8 @@
 from shiny import ui
 from shinywidgets import output_widget
 
+from ui.sidebar import sidebar
+
 def model_v5_tab():
     """
     Generate the UI layout for the Model V5 results tab.
@@ -16,23 +18,23 @@ def model_v5_tab():
     """
     return ui.nav_panel(
         "Current Model",
-        
-        ui.output_ui(id="selected_bird"),
-
-        ui.navset_card_underline(
-            ui.nav_panel(
-                "Map",
-                output_widget("map_widget"),
+        ui.layout_sidebar(
+            sidebar(),
+            ui.output_ui(id="selected_bird"),
+            ui.navset_card_underline(
+                ui.nav_panel(
+                    "Map",
+                    output_widget("map_widget"),
+                ),
+                ui.nav_panel(
+                    "Land Cover",
+                    ui.p("land_cover_placeholder"),
+                ),
+                ui.nav_panel(
+                    "Population Size",
+                    ui.output_data_frame("population_size"), 
+                ),
+                title="Model Results",
             ),
-            ui.nav_panel(
-                "Land Cover",
-                ui.p("land_cover_placeholder"),
-            ),
-            ui.nav_panel(
-                "Population Size",
-                # ui.p("population_size_placeholder"),
-                ui.output_data_frame("population_size"), 
-            ),
-            title="Model Results",
-        ),
+        )
     )
