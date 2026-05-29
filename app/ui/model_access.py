a2bb77cecb16b1fb75d6ede0f3ddd89cfb1cd010
Harrison Li <hli76@student.ubc.ca>
2026-05-29 12:59:39 -0700

    add vignettes to model access

--- app/ui/model_access.py
+++ app/ui/model_access.py
@@ -1,12 +1,27 @@
 from shiny import ui
 
-def model_access_tab():
+def vignette_panel(src, title):
+    return ui.nav_panel(
+        src.split("/")[-1],
+        ui.tags.iframe(
+            title,
+            src=src,
+            style="""
+                width:100%;
+                height:calc(100vh - 140px);
+                border:none;
+                display:block;
+            """
+        )
+    )
 
+def model_access_tab():
     return ui.nav_panel(
         "Model Access",
-        ui.card(
-            ui.card_header("Access our various model products", style="font-size:1.25rem"),
-            ui.p("A Boreal Avian Modelling Project"),
-            class_="model-access-card"
-        ),
+        ui.navset_tab(
+            vignette_panel("vignettes/BAMexploreR_1_intro.html", "Intro"),
+            vignette_panel("vignettes/BAMexploreR_2_access.html", "Access"),
+            vignette_panel("vignettes/BAMexploreR_3_distribution.html", "Distribution"),
+            vignette_panel("vignettes/BAMexploreR_4_habitat.html", "Habitat"),
+        )
     )
